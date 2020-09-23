1efce46ee00d84c7ea3a68c724fe6b34f56f39e3
Guilherme Pelusi <guilherme.pelusi@gmail.com>
2020-09-23 15:37:00 -0300

    clinic model

---
+++ app/models/clinic.rb
@@ -0,0 +1,6 @@
+class Clinic < ApplicationRecord
+  belongs_to :user
+  validates :name, presence: true, uniqueness: true
+  validates :address, presence: true
+  # has_many :vaccines
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :clinic
+  has_one :clinic, dependent: :destroy
 end
