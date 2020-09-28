6688134b795a80c584bda32add20a3530820063b
Guilherme Pelusi <guilherme.pelusi@gmail.com>
2020-09-27 23:40:30 -0300

    botões que crescem na tela principal

--- app/models/vaccine_type.rb
+++ app/models/vaccine_type.rb
@@ -2,4 +2,6 @@ class VaccineType < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
   has_many :vaccine, dependent: :destroy
+  include PgSearch::Model
+
 end
